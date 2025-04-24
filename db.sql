521de812be62f6459e876b3cae14148d0ee7d123
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-24 15:27:47 +0100

    la création de la table categories dans la base de données

--- db.sql
+++ db.sql
@@ -14,13 +14,10 @@ CREATE TABLE users (
 
 
 
+CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    stock INT NOT NULL,
-    page_count INT NOT NULL,
-    genre VARCHAR(100) NOT NULL,
-    author VARCHAR(100) NOT NULL,
-    category_id INT,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    name VARCHAR(100) NOT NULL
 );
+
+
 
