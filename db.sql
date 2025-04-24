d3ae779e75792a6004e88d9e03b7636fe0136715
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-24 15:27:00 +0100

    la création de la table users dans la base de données

--- db.sql
+++ db.sql
@@ -4,5 +4,23 @@ CREATE DATABASE devbook;
 
 USE devbook;
 
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
 
+
+
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    stock INT NOT NULL,
+    page_count INT NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
 
