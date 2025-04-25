09f2ee46e37496de63508fc16c22e668c5f478ce
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-25 16:28:27 +0100

    L'ajout des routes de connexion et d'inscription dans le web.js

---
+++ routes/web.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+const BookController = require('../controllers/BookController');
+const path = require('path');
+
+// Page home
+router.get('/', (req, res) => {
+    res.render('home', { user: req.session.user || null });
+  });
+  
+// Page d'inscription
+router.get('/register', (req, res) => {
+  res.render('register'); // Vue d'inscription
+});
+
+// Page de connexion
+router.get('/login', (req, res) => {
+  res.render('login'); // Vue de connexion
+});
+
+// Inscription
+router.post('/register', UserController.register);
+
+
+// Connexion
+router.post('/login', UserController.login);
+
+// Déconnexion
+router.get('/logout', UserController.logout);
+
+
+
+module.exports = router;
