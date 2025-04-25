652d181a6d288b7cfff05aef6cb45c8dfcbae01f
AsmaeElHamzaoui <asmaeelhamzaoui77@gmail.com>
2025-04-25 16:38:44 +0100

    l'implémentation de la fonction getCategoryById  dans la classe Categorie

--- models/Categorie.js
+++ models/Categorie.js
@@ -7,7 +7,13 @@ class Category {
     return rows;
   }
 
+  // Récupérer une catégorie par son ID
+  static async getCategoryById(id) {
+    const [rows] = await db.execute('SELECT * FROM categories WHERE id = ?', [id]);
+    return rows[0];
+  }
 
+ 
 }
 
 module.exports = Category;
